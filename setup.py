761cb4ba7c1271c52100d3fd65f3252a26257340
ariel <arielgordon123@gmail.com>
2020-02-11 03:36:47 +0200

    change P1 dependecy structure

--- setup.py
+++ setup.py
@@ -13,7 +13,7 @@ with open(path.join(here, 'README.md'), encoding='utf-8') as f:
 
 setup(
     name='P2',  # Required
-    version='0.0.8',  # Required
+    version='0.0.10',  # Required
     description='A sample P2',  # Optional
     long_description=long_description,  # Optional
     long_description_content_type='text/markdown',  # Optional (see note above)
@@ -33,7 +33,7 @@ setup(
     python_requires='>=3.5',
     install_requires=['boto3'],  # Optional
     dependency_links=[
-        'https://github.com/Arielgordon123/P1/releases/download/v0.0.6/P1-0.0.6-py3-none-any.whl'
+        "P1~0.0.6"
     ],
     entry_points={  # Optional
         'console_scripts': [
